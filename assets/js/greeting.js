9f346be721fb342eb8fa12164ac6f690ad310731
Luna <98078786+chalkprince@users.noreply.github.com>
2023-08-01 09:37:28 +0900

    Update greeting.js

--- assets/js/greeting.js
+++ assets/js/greeting.js
@@ -13,11 +13,11 @@ const gree3 = `${CONFIG.greetingAfternoon}\xa0`;
 const gree4 = `${CONFIG.greetingEvening}\xa0`;
 
 if (hour >= 23 || hour < 5) {
-	document.getElementById('greetings').innerText = gree1 + name;
+	document.getElementById('greetings').innerText = gree1; // add ' + name' if name wanted
 } else if (hour >= 6 && hour < 12) {
-	document.getElementById('greetings').innerText = gree2 + name;
+	document.getElementById('greetings').innerText = gree2;
 } else if (hour >= 12 && hour < 17) {
-	document.getElementById('greetings').innerText = gree3 + name;
+	document.getElementById('greetings').innerText = gree3;
 } else {
-	document.getElementById('greetings').innerText = gree4 + name;
+	document.getElementById('greetings').innerText = gree4;
 }
